962eddb51ed1054eb0e6bb1904d94bfe8136e062
Nakrani Drashti <nakranidrashti1@gmail.com>
2026-04-17 02:18:10 -0400

    Set urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('edit-profile', views.profile_view, name='profile'),
-    path('forgot-password/', views.forgot_password, name='forgot_password'),
+     path('forgot-password/', views.forgot_password, name='forgot_password'),
 ] 
